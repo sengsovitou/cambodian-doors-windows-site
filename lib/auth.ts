19c7803bf4b19005e695ef13dd5296775e6cd68e
TS <sengsovitou13@gmail.com>
2026-07-03 15:17:24 +0800

    Add secret & baseURL to better-auth config to resolve production errors

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,8 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
