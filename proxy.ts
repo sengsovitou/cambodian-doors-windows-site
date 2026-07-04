0340b03acca18f15393f32fea4a7d87c30c75216
TS <sengsovitou13@gmail.com>
2026-07-04 12:20:56 +0800

    Add NextResponse import to fix TS build error

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,6 @@ import { routing } from "./i18n/routing";
 import { NextRequest, NextResponse } from "next/server";
 
 export default function proxy(request: NextRequest) {
-  // Skip i18n proxy for all API endpoints
   if (request.nextUrl.pathname.startsWith("/api/")) {
     return NextResponse.next();
   }
