16e1810e3c405c5841f55c7371f648fb32d7515b
TS <sengsovitou13@gmail.com>
2026-07-04 13:03:16 +0800

    Remove invalid sslrootcert param, clean pg pool SSL config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -10,11 +10,8 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is missing");
 }
 
-// Append sslmode=require to connection string URL only
-const fullDbUrl = `${process.env.DATABASE_URL}?sslmode=require`;
-
 const pool = new Pool({
-  connectionString: fullDbUrl,
+  connectionString: process.env.DATABASE_URL,
   connectionTimeoutMillis: 30000,
   idleTimeoutMillis: 30000,
   max: 10,
