d67760b2c28aeb77fa386275b8702f8786b7c4d6
TS <sengsovitou13@gmail.com>
2026-07-04 13:29:28 +0800

    fix prisma config and build script

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,10 @@
+import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL, // Read from Vercel's environment
+  },
   migrations: {
     seed: "npx tsx ./prisma/seed.ts",
   },
